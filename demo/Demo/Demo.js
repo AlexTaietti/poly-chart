49d5f9d731acfa1595e61a66e92cb57a7d861799
AlexTaietti <taiettialex@yahoo.it>
2021-02-23 23:07:40 +0000

    [docs] change the example used to showcase most of the component's functionality

--- demo/Demo/Demo.js
+++ demo/Demo/Demo.js
@@ -14,21 +14,21 @@ export const Demo = () => {
    //could be extracted from some prop
    const demoData = [{
       COOL: 200,
-      LAME: 10,
+      LOVE: 10,
       WOW: 150,
       THINGS: 170,
       STUFF: 100,
       HAPPY: 190
    }, {
       COOL: 100,
-      LAME: 100,
+      LOVE: 100,
       WOW: 100,
       THINGS: 30,
       STUFF: 302,
       HAPPY: 200
    }, {
       COOL: 150,
-      LAME: 200,
+      LOVE: 200,
       WOW: 100,
       THINGS: 150,
       STUFF: 100,
@@ -42,14 +42,14 @@ export const Demo = () => {
 
       animation: {
          tween: true,
-         delay: [1000, 2000, 1000],
-         duration: 500,
-         easingFunction: 'easeOutElastic'
+         delay: [0, 500, 1000],
+         duration: [2000, 1000, 5000],
+         easingFunction: ['easeOutElastic', 'easeOutBounce', 'linear']
       },
 
       style: {
 
-         label: { font: `${12.5 * window.devicePixelRatio}px serif` },
+         label: { font: `${12.5 * window.devicePixelRatio}px Helvetica` },
 
          chart: {
             background: true,
@@ -59,8 +59,8 @@ export const Demo = () => {
 
          polygon: {
             contour: true,
-            fill: "rgba(255, 0, 0, 0.3)",
-            stroke: 'rgba(255, 0, 0, 1)',
+            fill: ['rgba(3, 5, 183, 0.6)', 'rgba(255, 0, 0, 0.6)', 'rgba(0, 210, 79, 0.6)'],
+            stroke: ['rgb(3, 5, 183)', 'rgb(255, 0, 0)', 'rgb(0, 210, 79)'],
             lineWidth: 2
          }
 
