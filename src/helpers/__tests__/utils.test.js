cf40038b59b9731326be9b8940dceacbbdd7a889
AlexTaietti <taiettialex@yahoo.it>
2021-02-24 15:18:06 +0000

    [test] add tests for the createFittingCanvas and getLongestStringWidth utilities

--- src/helpers/__tests__/utils.test.js
+++ src/helpers/__tests__/utils.test.js
@@ -1,20 +1,20 @@
-import { mergeObjects, getMaxValueInArray } from "../utils";
+import { mergeObjects, createFittingCanvas, getLongestStringWidth, getMaxValueInArray } from "../utils";
 
 describe('getMaxValueInArray: returns the highest value in an array of numbers', () => {
 
-   test('Returns the highest value in one-dimensional array of integers', () => {
+   test('Return the highest value in one-dimensional array of integers', () => {
 
-      const array = [1000, 2000, 1000000, 1, 200, 100, 420, 69];
+      const numbers = [1000, 2000, 1000000, 1, 200, 100, 420, 69];
 
-      expect(getMaxValueInArray(array)).toBe(1000000);
+      expect(getMaxValueInArray(numbers)).toBe(1000000);
 
    });
 
-   test('Returns the highest value in two-dimensional array of integers', () => {
+   test('Return the highest value in two-dimensional array of integers', () => {
 
-      const array = [1000, [2000, 200, 2, 100, 10000, 300000000], 1000000, 1, 200, 100, 420, 69];
+      const numbers = [1000, [2000, 200, 2, 100, 10000, 300000000], 1000000, 1, 200, 100, 420, 69];
 
-      expect(getMaxValueInArray(array)).toBe(300000000);
+      expect(getMaxValueInArray(numbers)).toBe(300000000);
 
    });
 
@@ -22,7 +22,7 @@ describe('getMaxValueInArray: returns the highest value in an array of numbers',
 
 describe('mergeObjects: returns a merged object', () => {
 
-   test('Merges two shallow objects', () => {
+   test('Merge two shallow objects', () => {
 
       const firstObject = { a: 100, b: 'hello', c: 'world', d: 1, e: 2020 };
 
@@ -40,7 +40,7 @@ describe('mergeObjects: returns a merged object', () => {
 
    });
 
-   test('Merges two objects with complex values', () => {
+   test('Merge two objects with complex values', () => {
 
       const firstObject = {
 
@@ -70,6 +70,44 @@ describe('mergeObjects: returns a merged object', () => {
       }
 
       expect(mergeObjects(firstObject, secondObject)).toEqual(expectedResult);
+
+   });
+
+});
+
+describe('getLongestStringWidth: return the width of the longest string from an array of strings based on its length when rendered in a canvas', () => {
+
+   test('return the width of the longest string', () => {
+
+      const longestString = 'supremelyUnecessarilyLongString';
+      const secondLongest = 'unecessarilyLongString';
+
+      const strings = ['normal', 'strings', longestString, 'some', 'more', 'strings'];
+      const moreStrings = ['normal', 'strings', secondLongest, 'some', 'more', 'strings'];
+
+      const testFont = '20px sans-serif';
+
+      const expectedMaxLength = getLongestStringWidth(testFont, strings);
+      const expectedShorterLength = getLongestStringWidth(testFont, moreStrings);
+
+      expect(expectedMaxLength).toBeGreaterThan(expectedShorterLength);
+
+   });
+
+});
+
+describe('createFittingCanvas: creates a canvas that fits inside a supplied HTML element, appends it to it. Returns a reference to said element, a 2d context and the fitted canvas', () => {
+
+   test("returns the same container passed to the function originally", () => {
+
+      const mockContainerElement = document.createElement('div');
+
+      mockContainerElement.style.width = "500px";
+      mockContainerElement.style.height = "200px";
+
+      const [referenceToMockContainer, ...rest] = createFittingCanvas(mockContainerElement);
+
+      expect(referenceToMockContainer).toBe(mockContainerElement);
 
    });
 
