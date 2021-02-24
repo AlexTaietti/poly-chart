64b35aa407ac2b6130d29cc205276942d472312c
AlexTaietti <taiettialex@yahoo.it>
2021-02-24 10:46:38 +0000

    [fix] added "externals" property to the production webpack configuration to avoid bundling react and react-dom

--- webpack.config.js
+++ webpack.config.js
@@ -67,6 +67,21 @@ const prodConfig = {
       })],
    },
 
+   externals: {
+      react: {
+         commonjs: 'react',
+         commonjs2: 'react',
+         amd: 'React',
+         root: 'React',
+      },
+      'react-dom': {
+         commonjs: 'react-dom',
+         commonjs2: 'react-dom',
+         amd: 'ReactDOM',
+         root: 'ReactDOM',
+      },
+   }
+
 };
 
 module.exports = [
